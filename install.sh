7aef5f0d383ff4e2112ddb6bcade366eff9a1f7a
Ahmed Jafri <me@ahmedjafri.com>
2018-12-19 09:18:28 -0800

    incremental commit

---
+++ install.sh
@@ -0,0 +1,14 @@
+# Install the extensions and settings I use for my vscode
+# This script works if the `code` command is in your $PATH
+
+# Generated extension list by using the following script:
+# $ `code --list-extensions | xargs -L 1 echo code --install-extension`
+code --install-extension eamodio.gitlens
+code --install-extension esbenp.prettier-vscode
+code --install-extension mitaki28.vscode-clang
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-vscode.Go
+code --install-extension slevesque.vscode-hexdump
+code --install-extension vsmobile.vscode-react-native
+code --install-extension wayou.vscode-todo-highlight
+
